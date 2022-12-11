a7d92a44a2f35457405f4784db3922e9cae49ece
xty9501 <111770330+xty9501@users.noreply.github.com>
2022-12-11 05:17:46 -0500

    Update contract.sol
    add "minCreditScore" variable and a "setMinCreditScore" function(only can be called by lender). The "applyForLoan" function now includes a check to ensure that the borrower has a credit score that is equal to or greater than the minimum credit score before granting the loan.

--- contract.sol
+++ contract.sol
@@ -1,28 +1,51 @@
 pragma solidity ^0.8.7;
+
 contract P2PLending {
+    // Struct to store information about a loan
     struct Loan {
         address borrower;
         uint amount;
         uint interestRate;
         uint repaymentPeriod;
+        uint creditScore;
     }
 
     address public lender;
     mapping(address => Loan) public loans;
 
+    // Minimum credit score required to apply for a loan
+    uint public minCreditScore;
+
     constructor(address _lender) public {
         lender = _lender;
     }
 
-    function applyForLoan(uint _amount, uint _interestRate, uint _repaymentPeriod) public {
+    function setMinCreditScore(uint _minCreditScore) public {
+        // Only the lender can set the minimum credit score
+        require(msg.sender == lender, "Only the lender can set the minimum credit score.");
+
+        // Set the minimum credit score
+        minCreditScore = _minCreditScore;
+    }
+
+    function applyForLoan(
+        uint _amount,
+        uint _interestRate,
+        uint _repaymentPeriod,
+        uint _creditScore
+    ) public {
         // Check if the borrower already has an active loan
         require(loans[msg.sender].amount == 0, "You already have an active loan.");
+
+        // Check if the borrower has a sufficient credit score
+        require(_creditScore >= minCreditScore, "Your credit score is not sufficient to apply for a loan.");
 
         // Create a new loan for the borrower
         loans[msg.sender].borrower = msg.sender;
         loans[msg.sender].amount = _amount;
         loans[msg.sender].interestRate = _interestRate;
         loans[msg.sender].repaymentPeriod = _repaymentPeriod;
+        loans[msg.sender].creditScore = _creditScore;
     }
 
     function repayLoan(uint _amount) public {
@@ -39,13 +62,11 @@ contract P2PLending {
         loan.amount -= _amount;
     }
 
-    function getLoan(address _borrower) public view returns (uint, uint, uint) {
+    function getLoan(address _borrower) public view returns (uint, uint, uint, uint) {
         // Get the loan information
         Loan memory loan = loans[_borrower];
 
         // Return the loan details
-        return (loan.amount, loan.interestRate, loan.repaymentPeriod);
+        return (loan.amount, loan.interestRate, loan.repaymentPeriod, loan.creditScore);
     }
-}
-
 }
