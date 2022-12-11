012432e1756c7feb2b131830212b0c9dc559c332
xty9501 <111770330+xty9501@users.noreply.github.com>
2022-12-11 04:59:48 -0500

    Update contract.sol
    add comments

--- contract.sol
+++ contract.sol
@@ -1,5 +1,4 @@
 pragma solidity ^0.8.7;
-
 contract P2PLending {
     struct Loan {
         address borrower;
@@ -16,7 +15,10 @@ contract P2PLending {
     }
 
     function applyForLoan(uint _amount, uint _interestRate, uint _repaymentPeriod) public {
+        // Check if the borrower already has an active loan
         require(loans[msg.sender].amount == 0, "You already have an active loan.");
+
+        // Create a new loan for the borrower
         loans[msg.sender].borrower = msg.sender;
         loans[msg.sender].amount = _amount;
         loans[msg.sender].interestRate = _interestRate;
@@ -24,14 +26,26 @@ contract P2PLending {
     }
 
     function repayLoan(uint _amount) public {
+        // Get the loan information
         Loan memory loan = loans[msg.sender];
+
+        // Check if the borrower has an active loan
         require(loan.amount > 0, "You do not have an active loan.");
+
+        // Check if the amount being repaid is less than or equal to the outstanding loan amount
         require(loan.amount >= _amount, "The amount you are trying to repay is higher than your outstanding loan.");
+
+        // Repay the loan
         loan.amount -= _amount;
     }
 
     function getLoan(address _borrower) public view returns (uint, uint, uint) {
+        // Get the loan information
         Loan memory loan = loans[_borrower];
+
+        // Return the loan details
         return (loan.amount, loan.interestRate, loan.repaymentPeriod);
     }
+}
+
 }
